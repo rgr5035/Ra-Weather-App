70a8c804c8aa1fedd1109dff92a533725f8b232b
Robby Ray <rgr5035@gmail.com>
2020-10-28 20:53:02 -0600

    worked on fetch API

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 var cityButtonEl = document.getElementById("city-form");
 cityInputEl = document.getElementById("city-input");
+citynamesearchEl = document.getElementById("city-search-term");
 
 
 submitCity = function(e) {
     e.preventDefault();
     console.log('clicked');
 
-    var cityInput = cityInput.value.trim();
+    var cityInput = cityInputEl.value.trim();
+
+    if (cityInput) {
+        getCityInput(cityInput);
+
+    }
 }
 
 
+var getCityInput = function (city) {
 
+    apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=cb9f638b983772109f5be92fa81ecd11'
+
+    fetch(apiURL)
+     .then(function (response){
+
+      if(response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+            citynamesearchEl.textContent = data[i].name;
+
+        });
+
+      }
+
+        })
+
+}
 
 
 
