390b323dd3eb511143eef3d1bf6b1d3eba7111d4
Robby Ray <rgr5035@gmail.com>
2020-10-28 20:03:40 -0600

    worked on event handler for city submit

---
+++ script.js
@@ -0,0 +1,29 @@
+var cityButtonEl = document.getElementById("city-form");
+cityInputEl = document.getElementById("city-input");
+
+
+submitCity = function(e) {
+    e.preventDefault();
+    console.log('clicked');
+
+    var cityInput = cityInput.value.trim();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cityButtonEl.addEventListener('click', submitCity);
