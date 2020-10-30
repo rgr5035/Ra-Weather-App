3f1193db7a729f354f0abc87b33b0483e6eec4bd
Robby Ray <rgr5035@gmail.com>
2020-10-29 18:40:33 -0600

    created UV index info and icon info in JS

--- script.js
+++ script.js
@@ -3,56 +3,105 @@ var cityButtonEl = document.getElementById("search-btn");
 var cityInputEl = document.getElementById("city-input");
 var currentweathercardEl = document.getElementById("current-weather-card");
 var citynamesearchEl = document.getElementById("city-search-term");
+var iconimgEl = document.getElementById("icon-img");
 var tempsearchEl = document.getElementById("temperature");
 var humiditysearchEL = document.getElementById("humidity");
 var windspeedsearchEL = document.getElementById("wind-speed");
 var uvindexEL = document.getElementById("uv-index");
 
+submitCity = function (e) {
+  e.preventDefault();
+  console.log("clicked");
 
-submitCity = function(e) {
-    e.preventDefault();
-    console.log('clicked');
+  var cityInput = cityInputEl.value.trim();
 
-    var cityInput = cityInputEl.value.trim();
-
-    if (cityInput) {
-        getCityInput(cityInput);
-
-    }
-}
-
+  if (cityInput) {
+    getCityInput(cityInput);
+    getfivedayInput(cityInput);
+  }
+};
 
 var getCityInput = function (city) {
+  currentWeatherapiURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=cb9f638b983772109f5be92fa81ecd11";
 
-    apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=cb9f638b983772109f5be92fa81ecd11'
+  fetch(currentWeatherapiURL).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
 
-    
+        var cityLat = data.coord.lat;
+        var cityLon = data.coord.lon;
 
-    fetch(apiURL)
-     .then(function (response){
+        uvindexapiURL =
+          "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+          cityLat +
+          "&lon=" +
+          cityLon +
+          "&appid=cb9f638b983772109f5be92fa81ecd11";
 
-      if(response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
+        fetch(uvindexapiURL).then(function (response) {
+          if (response.ok) {
+            console.log(response);
+            response.json().then(function (datatwo) {
+              console.log(datatwo);
 
-            // var getIcon = data.weather[0].icon;
-            // console.log(getIcon);
-            
-            // icon = 'http://openweathermap.org/img/wn/' + getIcon + '@2x.png';
+              var getIcon = data.weather[0].icon;
+              console.log(getIcon);
 
-            currentweathercardEl.classList.remove('hide');
-            citynamesearchEl.textContent = data.name + ": " + moment().format("dddd, MMMM Do YYYY");
-            tempsearchEl.textContent = "Temperature: " + data.main.temp + " " + String.fromCharCode(176) + "F";
-            humiditysearchEL.textContent = "Humidity: " + data.main.humidity + "%";
-            windspeedsearchEL.textContent = "Wind Speed: " + data.wind.speed + " MPH";
-            uvindexEL.textContent = "UV Index:";
+              icon = "http://openweathermap.org/img/wn/" + getIcon + "@2x.png";
+
+              currentweathercardEl.classList.remove("hide");
+              citynamesearchEl.textContent =
+                data.name + ": " + moment().format("dddd, MMMM Do YYYY");
+
+              iconimgEl.setAttribute("src", icon);
+
+              tempsearchEl.textContent =
+                "Temperature: " +
+                data.main.temp +
+                " " +
+                String.fromCharCode(176) +
+                "F";
+              humiditysearchEL.textContent =
+                "Humidity: " + data.main.humidity + "%";
+              windspeedsearchEL.textContent =
+                "Wind Speed: " + data.wind.speed + " MPH";
+              uvindexEL.textContent = "UV Index: " + datatwo.value;
+
+              uvindexapiURL =
+                "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+                cityLat +
+                "&lon=" +
+                cityLon +
+                "&appid=cb9f638b983772109f5be92fa81ecd11";
+            });
+          }
         });
+      });
+    }
+  });
+};
 
-      }
+var getfivedayInput = function (city) {
+  fivedayapiURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&&appid=cb9f638b983772109f5be92fa81ecd11";
 
-        })
+  fetch(fivedayapiURL).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (datathree) {
+        console.log(datathree);
 
-}
+        //FOR LOOP FOR DATA I NEED IN URL
+      });
+    }
+  });
+};
 
-cityButtonEl.addEventListener('click', submitCity);
+cityButtonEl.addEventListener("click", submitCity);
